e0663bb1357c834bad3b3e066b6385a80c1ee4cc
lucasfrct <lucasfrct@gmail.com>
2022-04-30 01:47:00 -0300

    update

--- src/main/java/com/environment/aplication/Query.java
+++ src/main/java/com/environment/aplication/Query.java
@@ -4,11 +4,37 @@ package com.environment.aplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 @RestController
 public class Query {
+
+    static final String DB_URL = "jdbc:oracle:thin:@172.16.0.55:1521:UISA";
+    static final String USER = "blockchain";
+    static final String PASS = "i4nENMW0R5fXuPLAADfD";
+    static final String QUERY = "select SAFRA from pimsprd.vw_uisa_blockchain_colheita";
     
     @PostMapping("/query")
     public String index() {
+
+        try(Connection conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(QUERY);
+        ) {		      
+            while(rs.next()){
+                //Display values
+                System.out.print("SAFRA: " + rs.getInt("SAFRA"));
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } 
+
+
         return "QUERY";
     }
 }
